ddffa60f34bdb15d4a7502dd32ee605fd34fbf33
Al Amin Chayan <al.amin.chayan@portonics.com>
2020-05-18 22:42:54 +0600

    fix missing controller bugs

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,15 +2,16 @@
 
 Route::name('menu-maker::')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
+    Route::redirect('/', 'roles/assign');
 
     Route::put('roles/assign', 'RoleController@assign')->name('roles.assign');
     Route::get('roles/menus', 'RoleController@menus')->name('roles.menus');
     Route::get('menus/{node}/tree', 'MenuController@tree')->name('menus.tree');
     Route::resources([
-        'roles'   => 'RoleController',
-        'sections' => 'SectionController',
-        'menus'    => 'MenuController'
-    ]);
+         'roles'   => 'RoleController',
+         'sections' => 'SectionController',
+         'menus'    => 'MenuController'
+     ]);
     Route::get('selected-menus', 'MenuController@selected')->name('menus.selected');
 
     Route::resource('users', 'UserController')->only(['index', 'edit', 'update']);
